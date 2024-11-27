ad94e01e1d81fa79f75b31ea6feefabaadfc2dbf
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2024-11-27 19:47:44 +0900

    mask generator 추가

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from ImageSet import ImageFolder
 
 import matplotlib.pyplot as plt
 
+import itertools
+
 import cv2 as cv
 import numpy as np
 
@@ -21,6 +23,7 @@ BATCH_SIZE = 10
 epochs = 300
 learning_rate = 0.002
 image_size = (3, 256, 256)
+guide_size = (4, 256, 256)
 
 # 손실 함수 정의
 criterion_adversarial = nn.MSELoss()  # Adversarial loss
@@ -28,14 +31,16 @@ criterion_identity = nn.L1Loss()    # identity_loss
 criterion_cycle = nn.L1Loss()
 
 # 모델 초기화
-generator_f = Generator(image_size).to(device)
-generator_s = Generator((4,256,256)).to(device)
-discriminator = Discriminator(image_size).to(device)
+generator_f2s = Generator(guide_size).to(device)
+generator_s2f = Generator(image_size).to(device)
+discriminator_s = Discriminator(image_size).to(device)
+discriminator_f = Discriminator(image_size).to(device)
 
 # 옵티마이저 정의
-optimizer_G = optim.Adam(list(generator_f.parameters())+list(generator_s.parameters()),
+optimizer_G = optim.Adam(itertools.chain(generator_f2s.parameters(), generator_s2f.parameters()),
                           lr=0.0002, betas=(0.5, 0.999))
-optimizer_D = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer_DA = optim.Adam(discriminator_f.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer_DB = optim.Adam(discriminator_s.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
 transform = transforms.Compose([
     transforms.Resize((256, 256)),  # 이미지 크기 조정
@@ -44,11 +49,24 @@ transform = transforms.Compose([
 dataset = ImageFolder(root='images/', transform=transform)
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
+import cv2
+gray = transforms.Grayscale()
+to_pil = transforms.ToPILImage()
+def mask_generator(shadow, free):
+    shadow = gray(shadow).squeeze(0) * 255
+    free = gray(free).squeeze(0) * 255
+    shadow_np = shadow.cpu().numpy().astype(np.uint8)
+    free_np = free.cpu().numpy().astype(np.uint8)
+
+    diff =  free_np-shadow_np
+    _, otsu = cv2.threshold(diff, -1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    return torch.from_numpy(otsu).float() / 255
+
 # 훈련 루프
 for epoch in range(epochs):
-    for i, (shadow, removed ,mask) in enumerate(dataloader):
+    for i, (shadow, free , mask) in enumerate(dataloader):
         shadow = shadow.to(device)
-        removed = removed.to(device)
+        free = free.to(device)
         mask = mask.to(device)
 
         # ---------------------
@@ -56,39 +74,22 @@ for epoch in range(epochs):
         # ---------------------
         optimizer_G.zero_grad()
 
-        # 그림자 제거 이미지 생성 - shadow cycle consistency loss
-        shadow_free = generator_f(shadow)
-        guided = torch.cat([shadow_free, mask], dim=1)
-        shadow_fake = generator_s(guided)
+        #identity loss
+        free_fake = generator_s2f(free) # ~If
+        identity_loss_free = criterion_identity(free, free_fake)
 
-        cycle_consist_loss = criterion_cycle(shadow_fake, shadow)
+        #GAN loss
+        output = discriminator_f(free_fake)
+        label = torch.ones_like(free_fake, requires_grad=False).to(device)
+        gan_loss_free = criterion_adversarial(label, output)
 
-        # shadow identity loss
-        guided = torch.cat([shadow, mask], dim=1)
-        shadow_fake = generator_s(guided)
-        identity_loss = criterion_identity(shadow_fake, shadow)
+        guided = torch.cat((free, mask), dim=1)
+        shadow_fake = generator_f2s(guided) # ~Is
+        output = discriminator_s(shadow_fake)
+        label = torch.zeros_like(output, requires_grad=False).to(device)
+        gan_loss_shadow = criterion_adversarial(label, output)
 
-        # ---------------------
-        # Discriminator 훈련
-        # ---------------------
-        optimizer_D.zero_grad()
-        predicted = discriminator(shadow_free.detach())
-
-        label = torch.zeros_like(predicted)
-
-        discriminate_loss = criterion_adversarial(predicted, label)
-
-        total_loss = cycle_consist_loss + identity_loss + discriminate_loss
-        total_loss.backward()
-        optimizer_G.step()
-        optimizer_D.step()
-
-        if i % 10 == 0:
-            print(f"Epoch [{epoch+1}/{epochs}], Batch [{i}/{len(dataloader)}]\n"
-                  f"cycle loss {cycle_consist_loss.item():.4f}, identity loss {identity_loss.item():.4f}, "
-                  f"discriminator loss: {discriminate_loss.item():.4f}")
-            transform = transforms.ToPILImage()
-            plt.imshow(transform(shadow_free[0]))
-            plt.show()
-
-torch.save(generator_f.state_dict(), 'generator.pth')
+        #shadow cycle_consistency
+        guided = torch.cat((free_fake, mask), dim=1)
+        shadow_fake_cycle = generator_f2s(guided)
+        consistency_loss_shadow = criterion_cycle(shadow_fake_cycle, shadow)
